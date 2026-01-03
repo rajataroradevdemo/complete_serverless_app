2206f4a827ab49eb700a8e4ef1a470b0633fe51a
Rajat Arora <rajat.aroramail@gmail.com>
2026-01-03 15:43:10 +0530

    adding update tutorial code

---
+++ serverless/update_tutorial_lambda.py
@@ -0,0 +1,35 @@
+import json
+import boto3
+from datetime import datetime
+
+def lambda_handler(event, context):
+
+   print("Event json %s" % json.dumps(event))
+   print("Context %s" % context)
+   
+   client = boto3.resource('dynamodb')
+
+   table = client.Table('tutorials')
+
+   eventDateTime = (datetime.now()).strftime("%Y-%m-%d %H:%M:%S")
+   published = False
+   
+   print("Updating record.")
+
+   response = table.put_item(
+       Item={
+           'id':  event['id'],
+           'title': event['title'],
+           'description': event['description'],
+           'published': event['published'],
+           'updatedAt': eventDateTime,
+           'createdAt': eventDateTime
+       }
+   )
+
+   print("Record updated successfully.")
+
+   return {
+       'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+       'body': 'Record ' +  context.aws_request_id + ' added'
+   }
