f5a574d2fa4733a0dee86c34747cc17ebb75a005
Rajat Arora <rajat.aroramail@gmail.com>
2026-01-03 23:20:09 +0530

    add get tutorial lambda

---
+++ serverless/get_tutorial_lambda.py
@@ -0,0 +1,32 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+   client = boto3.resource('dynamodb')
+
+   table = client.Table('tutorials')
+
+   response = table.get_item(
+
+       Key={
+
+           'id': event['id']
+
+       }
+
+   )
+
+   if 'Item' in response:
+
+       return response['Item']
+
+   else:
+
+       return {
+
+           'statusCode': '404',
+
+           'body': 'Not found'
+
+       }
